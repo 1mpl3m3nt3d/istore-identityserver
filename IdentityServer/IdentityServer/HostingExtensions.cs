d0d95a2e3b77b0e381513197e60e29f56d355956
1mpl3m3nt3d <105541001+1mpl3m3nt3d@users.noreply.github.com>
2022-09-20 23:42:06 +0300

    Update HostingExtensions.cs

--- IdentityServer/IdentityServer/HostingExtensions.cs
+++ IdentityServer/IdentityServer/HostingExtensions.cs
@@ -33,7 +33,7 @@ internal static class HostingExtensions
             {
                 options.Authentication.CookieSameSiteMode = SameSiteMode.Unspecified;
                 options.Cors.CorsPolicyName = "CorsPolicy";
-                //options.IssuerUri = builder.Configuration["IdentityUrl"];
+                options.IssuerUri = builder.Configuration["IdentityUrl"];
                 options.Events.RaiseErrorEvents = true;
                 options.Events.RaiseInformationEvents = true;
                 options.Events.RaiseFailureEvents = true;
