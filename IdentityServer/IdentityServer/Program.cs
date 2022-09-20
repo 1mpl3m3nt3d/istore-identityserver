beefdf7dce9517b5d871fd9cd7b818fcb0f844ef
1mpl3m3nt3d <105541001+1mpl3m3nt3d@users.noreply.github.com>
2022-09-20 18:04:24 +0300

    Fix

--- IdentityServer/IdentityServer/Program.cs
+++ IdentityServer/IdentityServer/Program.cs
@@ -18,7 +18,7 @@ try
         .ReadFrom.Configuration(ctx.Configuration));
 
     var configuration = new ConfigurationBuilder()
-    .SetBasePath(Directory.GetCurrentDirectory())
+    //.SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
     .AddEnvironmentVariables()
     .AddCommandLine(args)
